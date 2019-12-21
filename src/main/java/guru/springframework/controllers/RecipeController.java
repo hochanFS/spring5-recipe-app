d22ec09c27a9518f2f05414027d5dae94bcff8bc
Hochan Lee <hochan.lee@financescript.com>
2019-12-21 18:12:24 -0500

    Integrated the validator messages with viewer

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -20,9 +20,8 @@ import javax.validation.Valid;
 @Controller
 public class RecipeController {
 
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     private final RecipeService recipeService;
-
-    final String RECIPE_RECIPEERROR_URL = "recipe/recipeform";
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
@@ -31,7 +30,8 @@ public class RecipeController {
     @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
 
-        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+        model.addAttribute("recipe", recipeService.findById(new Long(id)));
+
         return "recipe/show";
     }
 
@@ -45,18 +45,19 @@ public class RecipeController {
     @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return  "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @PostMapping("recipe")
     public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
 
-        if (bindingResult.hasErrors()) {
+        if(bindingResult.hasErrors()){
+
             bindingResult.getAllErrors().forEach(objectError -> {
                 log.debug(objectError.toString());
             });
 
-            return RECIPE_RECIPEERROR_URL;
+            return RECIPE_RECIPEFORM_URL;
         }
 
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
@@ -75,13 +76,17 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception) {
+    public ModelAndView handleNotFound(Exception exception){
+
         log.error("Handling not found exception");
         log.error(exception.getMessage());
+
         ModelAndView modelAndView = new ModelAndView();
+
         modelAndView.setViewName("404error");
         modelAndView.addObject("exception", exception);
 
         return modelAndView;
     }
+
 }
